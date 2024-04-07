775eead61b653850a5607c1612cfc27ce9b10726
Adasat <pbonbol@gmail.com>
2024-04-07 20:42:08 +0100

    Changes in start project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,47 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import { onMounted, ref } from 'vue';
+
+
+const memes = ref([]) // Constante reactiva --> Es un objeto que su value es reactivo.
+
+const getAllMemes = async () => {
+  const response = await fetch('https://api.imgflip.com/get_memes')
+  const { data } = await response.json()
+
+  console.log(data)
+  memes.value = data.memes
+}
+getAllMemes()
+
+onMounted(() => { //Esto es un ciclo de vida
+  console.log('componente montado')
+})
 </script>
+
+<!-- <script>
+export default {
+  setup() {
+    import HelloWorld from './components/HelloWorld.vue'
+    import TheWelcome from './components/TheWelcome.vue'
+  }
+}
+</script> -->
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div v-for="meme in memes" :key="meme.id" class="card">
+        <img :src="meme.url" />
+        <h1>{{meme.name}}</h1>
+    
+      </div>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
@@ -25,6 +53,15 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+.card {
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+
+  width: 40px;
+  height: 60px;
 }
 
 @media (min-width: 1024px) {
